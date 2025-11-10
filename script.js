73693d54bffe51415bd6efd30921d9eef3bd6430
Bhuvan S Shetty <135850214+BhuvanSShetty@users.noreply.github.com>
2025-11-10 14:12:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Smooth scrolling for navigation
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', e => {
+    e.preventDefault();
+    document.querySelector(anchor.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
